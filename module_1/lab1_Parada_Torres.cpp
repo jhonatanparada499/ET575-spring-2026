8ca0c4c2c78acd1624c6fa254857254759accca1
Jhonatan Parada <119643678+jhonatanparada499@users.noreply.github.com>
2026-01-28 19:47:05 +0000

    feat: add lab assignment number 1

---
+++ module_1/lab1_Parada_Torres.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+
+#include <string>
+using std::string;
+
+int main(){
+    string country;
+    char gender;
+    int zipcode;
+    float rate;
+    
+    cout << "Enter country: ";
+    cin >> country;
+    cout << "Enter gender (f)emale, (m)ale or (o)thers : ";
+    cin >> gender;
+    cout << "Enter zipcode: ";
+    cin >> zipcode;
+    cout << "Enter tax rate: ";
+    cin >> rate;
+
+    cout << "\n";
+    cout << "Entered country: " << "\t" << country << "\n";
+    cout << "Selected gender: " << "\t" << gender << "\n";
+    cout << "Zip code: " << "\t\t" << zipcode << "\n";
+    cout << "Tax rate: " << "\t\t" << zipcode << "\n";
+
+    return 0;
+}
