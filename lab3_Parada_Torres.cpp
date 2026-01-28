61dc050723257af731321f4795a4e6e02de5fccd
Student <Student@et.qcc.edu>
2026-01-28 10:53:55 -0500

    feat: add lab 3 numerical variables

---
+++ lab3_Parada_Torres.cpp
@@ -0,0 +1,19 @@
+/*
+Jhonatan Stiven Parada Torres
+Jan 28, 2026
+Lab 3, Numerical Variables
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "\n---------- Example 1 ----------"<< endl;
+
+    int number = 10.9;
+    float n = 7.8;
+
+    cout << "number = " << number << endl;
+    cout << "n = " << n << endl;
+    return 0;
+}
