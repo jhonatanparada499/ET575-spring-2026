592040682df20bbb9a8df50f9ace40b44856703e
Jhonatan Parada <119643678+jhonatanparada499@users.noreply.github.com>
2026-01-28 16:21:34 +0000

    feat: add example 2 classwork

--- lab3_Parada_Torres.cpp
+++ lab3_Parada_Torres.cpp
@@ -15,5 +15,16 @@ int main(){
 
     float n = 7.8;
     cout << "n = " << n << endl;
+
+    cout << "---------- Example 2: Numberical Operators ----------"<< endl;
+
+    number = 5;
+    cout << "sum = "<< number + n << endl;
+
+    int number2 = 2;
+    // explicit casting from int to float
+    cout << "division = "<< 
+        number / static_cast<float>(number2) << endl;
+
     return 0;
 }
