d8fba73966d0b4f793e2f2a75f79d09d4bde5a80
Jhonatan Parada <jhonatanparada499@gmail.com>
2026-02-02 14:58:36 -0500

    feat: add lab exercise solution

--- module_1/lab_4/lab4_parada_torres.cpp
+++ module_1/lab_4/lab4_parada_torres.cpp
@@ -4,19 +4,17 @@ Feb 2, 2026
 Lab 4 exercise, String and String Methods
 */
 
-#include <iostream>
-using namespace std;
-
-#include <string>
 #include <cstdlib>
-
 #include <format>
+#include <iostream>
+#include <string>
+using namespace std;
 
 int main() {
   cout << "Example 1: String Indexing" << '\n';
   string firstName = "Peter";
 
-  cout << "The 4th char is "<< firstName[3] << '\n';
+  cout << "The 4th char is " << firstName[3] << '\n';
   cout << "The 3rd char is " << firstName.at(2) << '\n';
 
   cout << "Example 2: String Length" << '\n';
@@ -27,16 +25,16 @@ int main() {
   string color = "Orange";
   string animal = "animal";
   string animalAndColor = animal + color;
-  cout << "Concatenation result: "<< animalAndColor << '\n';
+  cout << "Concatenation result: " << animalAndColor << '\n';
 
   cout << "Example 4: String Substraction" << '\n';
   string city = "Bayside";
-  string sub_char = city.substr(3,2);
+  string sub_char = city.substr(3, 2);
   cout << "Substracted: " << sub_char << '\n';
 
   cout << "Example 5: String Insertion" << '\n';
   city.insert(3, "Queens-");
-  cout << "After insertion: "<< city << '\n';
+  cout << "After insertion: " << city << '\n';
 
   cout << "Example 6: String Append" << '\n';
   string fullName = "Jhonatan Parada";
@@ -45,26 +43,44 @@ int main() {
 
   cout << "Example 7: Char Replacement" << '\n';
   string word = "engineering";
-  word.replace(5,2,"FUTURE");
-  cout << "After char rep: "<< word << '\n';
+  word.replace(5, 2, "FUTURE");
+  cout << "After char rep: " << word << '\n';
 
   cout << "Example 8: String Erasement" << '\n';
   word = "layout";
   cout << "Original: " << word << '\n';
-  word.erase(2,3);
+  word.erase(2, 3);
   cout << "After: " << word << '\n';
 
   cout << "Example 9: String Finding" << '\n';
   word = "characters";
 
   int index = word.find('a');
-  cout << "Index for a: "<< index << '\n';
+  cout << "Index for a: " << index << '\n';
 
-  cout << "Exercise" << '\n';
+  cout << "---- Exercise ----" << '\n';
+  // Source: https://cplusplus.com/forum/beginner/283798/
 
-  cout << std::format("{:<10}\n", "Fruits");
+  // I am not using external libraries, all you need is
+  // to run:
+  //
+  // $ g++ -std=c++20 [source] -o [dest]
 
+  cout << "Type a word: ";
+  cin >> word;
+  cout << endl;
 
+  char secondChar = word[1];
+  size_t wordLenght = word.length();
+  string removedWord = word.substr(0, word.length() - 3);
+  string replacedWord = word.replace(2, 2, "--$--");
+
+  const int minWidth = 25;
+  // format '2nd characters' using the minWidth
+  cout << format("{:<{}}{}\n", "2nd character:", minWidth, secondChar);
+  cout << format("{:<{}}{} characters\n", "Word has:", minWidth, wordLenght);
+  cout << format("{:<{}}{}\n", "Replace word:", minWidth, replacedWord);
+  cout << format("{:<{}}{}\n", "Remove end characters:", minWidth, removedWord);
 
   return EXIT_SUCCESS;
 }
