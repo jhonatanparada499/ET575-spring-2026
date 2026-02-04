4f05f35b5f9f80b8d8642d94319be2b8eca81447
Jhonatan Parada <119643678+jhonatanparada499@users.noreply.github.com>
2026-02-04 16:02:57 +0000

    feat: add result activity 2 solution

--- module_1/homework_1/parada_torres.cpp
+++ module_1/homework_1/parada_torres.cpp
@@ -3,8 +3,6 @@
 //
 // Student's Name: Jhonatan Parada Torres
 //
-//
-//
 
 #include <iostream>
 #include <string>
@@ -33,15 +31,56 @@ int main() {
   cout << "Message length: " << "\t\t" <<
   msg_lenght << '\n';
 
-  if (msg_index == -1) {
-     return 0;
+  if (msg_index != -1) {
+     cout << "Index of character 'ma': " << '\t' <<
+     msg_index << '\n';
+
+     cout << "Replace 'ma' with %---%: " << '\t' <<
+     msg.replace(msg_index, 2, "%---%") << endl;
   }
 
-  cout << "Index of character 'ma': " << '\t' <<
-  msg_index << '\n';
 
-  cout << "Replace 'ma' with %---%: " << '\t' <<
-  msg.replace(msg_index, 2, "%---%") << endl;
+  constexpr double PI = 3.14159;
+  float height, radius;
+
+  cout << "Enter Height: ";
+  cin >> height;
+  cout << "Enter Radius: ";
+  cin >> radius;
+
+  bool radiusGreaterHeight = radius > height;
+  bool radiusEqualHeight = radius == height;
+
+  height *= 3;
+  radius += 5;
+
+  bool radiusNotEqualAndGreaterHeight = radius != height and radius > height;
+
+  float cylinderVolume = PI * (radius * radius) * height;
+
+  cout << "------------------------------ RESULT ACTIVITY 2 "
+          "----------------------------"
+       << '\n';
+
+  cout << "Step 3) Is the radius greater than height? " << "\t\t\t\t\t" <<
+  radiusGreaterHeight << '\n';
+
+  cout << "Step 4) Is the radius equal to height? " << "\t\t\t\t\t\t" <<
+  radiusEqualHeight << '\n';
+   
+  cout << "Step 5) Triple height = " << "\t\t\t\t\t\t\t" <<
+  height << '\n';
+
+  // Error in instructions, repeated step number
+  cout << "Step 5) Increased radius by 5 = " << "\t\t\t\t\t\t" <<
+  radius << '\n';
+
+  cout << "Step 6) Is the radius not equal to and greater than height ? " << "\t\t\t" <<
+  radiusNotEqualAndGreaterHeight << '\n';
+
+  cout << "Step 7) The volume of a right cylinder with radius " << 
+  radius << "cm and height " << height << "cm is =" << "\t" <<
+  cylinderVolume << "cm^3" << endl;
 
   return 0;
 }
