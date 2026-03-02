79cd99f8a6be256ccc1130d2d68b2a77af0919b6
Jhonatan Parada <119643678+jhonatanparada499@users.noreply.github.com>
2026-03-02 15:49:40 +0000

    feat: add lab 9 dir

---
+++ module_3/lab_9/Lab9_Parada_Torres.cpp
@@ -0,0 +1,21 @@
+/*
+Jhonatan Stiven Parada Torres
+March 2, 2026
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  cout << "------ EXAMPLE 1 ------" << '\n';
+
+  for(int i = 0; i < 5; ++i){
+    cout << "Hello" << i << '\n';
+  }
+
+  cout << "------ EXAMPLE 2 ------" << '\n';
+
+  return 0;
+}
