f5d96bff0f8c17cbf604de54dfcf684f9bcb9b9f
Jhonatan Parada <119643678+jhonatanparada499@users.noreply.github.com>
2026-03-09 14:26:42 +0000

    feat: add lab11 class exercise

---
+++ module_3/lab_9/lab11_Parada_Torres.cpp
@@ -0,0 +1,35 @@
+/*
+Jhonatan Stiven Parada Torres
+March 9, 2026
+*/
+
+#include <iostream>
+// #include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::cerr;
+
+int main()
+{
+  cout << "------ EXAMPLE 1 ------" << '\n';
+  int i = 0;
+  while (i<=5)
+  {
+    cout << i << '\t';
+    ++i;
+  }
+
+  cout << "------ EXAMPLE 2 ------" << '\n';
+
+  cout << "------ EXAMPLE 3 ------" << '\n';
+
+  cout << "------ EXAMPLE 5 ------" << '\n';
+
+  cout << "------ EXERCISE 1 ------" << '\n'; 
+
+  cout << "------ EXERCISE 2 ------" << '\n';
+
+  return 0;
+}
