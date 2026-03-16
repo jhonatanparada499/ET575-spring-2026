7f559cbfeb05508efbb3d429ba5d8f50516f8d14
Student <Student@et.qcc.edu>
2026-03-16 11:02:45 -0400

    feat: move lab13 files to unique dir

---
+++ module_4/lab_13/lab13_main_paradatorres.cpp
@@ -0,0 +1,19 @@
+/*
+Jhonatan Stiven Parada Torres
+March 16, 2026
+*/
+
+#include <iostream>
+// #include <string>
+#include "lab13_function_paradatorres.cpp"
+
+using std::cerr;
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+    cout << "EXAMPLE 1" << '\n';
+    printHello();
+    return 0;
+}
