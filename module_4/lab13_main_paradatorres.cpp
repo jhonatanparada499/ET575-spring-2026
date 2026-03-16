2c79330af723732e3eb083415e9e02130a14bc74
Student <Student@et.qcc.edu>
2026-03-16 10:59:00 -0400

    feat: add printHello function

---
+++ module_4/lab13_main_paradatorres.cpp
@@ -0,0 +1,18 @@
+/*
+Jhonatan Stiven Parada Torres
+March 16, 2026
+*/
+
+#include <iostream>
+// #include <string>
+#include "lab13_function_paradatorres.cpp"
+
+using std::cerr;
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+    cout << "EXAMPLE 1" << '\n';
+    return 0;
+}
