0ecfb286d2add0ffa2d91d60c463ab21bfc8cc23
Student <Student@et.qcc.edu>
2026-03-16 11:24:00 -0400

    feat: add greeting, tripple and iseven func defs

--- module_4/lab_13/lab13_function_paradatorres.cpp
+++ module_4/lab_13/lab13_function_paradatorres.cpp
@@ -4,7 +4,7 @@ March 16, 2026
 */
 
 #include <iostream>
-// #include <string>
+#include <string>
 
 using std::cerr;
 using std::cin;
@@ -13,4 +13,26 @@ using std::endl;
 
 void printHello() {
     cout << "Hello World" << '\n';
+}
+
+void greeting(std::string username){
+    cout << "Welcome to function " 
+         << username
+         << '\n';
+}
+
+int tripleNum(int theNum){
+    return theNum*3;
+}
+
+bool isEven(int theNum){
+    return !(theNum % 2);
+}
+
+// EXERCISE
+void printIsEven(int theNum){
+    cout << "Is " << std::to_string(theNum)
+         << " even? "
+         << (isEven(theNum)? "True":"False")
+         << endl;
 }
