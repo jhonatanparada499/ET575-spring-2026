a14737ae477f436a9d5c3e00acb0a3fffe742832
Jhonatan Parada <jhonatanparada499@gmail.com>
2026-03-18 12:23:36 -0400

    feat: add printDistance function

--- module_4/lab_14/lab14_main_paradatorres.cpp
+++ module_4/lab_14/lab14_main_paradatorres.cpp
@@ -13,25 +13,26 @@ using std::cout;
 using std::endl;
 
 int main() {
-    cout << "EXAMPLE 1" << '\n';
-    int calc = calculatedb(2,3);
-    cout << calc << '\n';
+  cout << "EXAMPLE 1" << '\n';
+  int calc = calculatedb(2, 3);
+  cout << calc << '\n';
 
-    cout << "EXAMPLE 2" << '\n';
-    cheer(4);
+  cout << "EXAMPLE 2" << '\n';
+  cheer(4);
 
-    cout << "EXAMPLE 3" << '\n';
-    int n = linearFunc(3);
-    cheer(4);
-    cout << "Final result: "<< n << '\n';
-    
-    cout << "EXAMPLE 4" << '\n';
-    int s1 = collectNumber();
-    int s2 = collectNumber();
-    float h = hypotenuse(s1,s2);
-    printResult(s1,s2,h);
-    
-    cout << "EXERCISE" << '\n';
-    cout << "" << '\n';
-    return 0;
+  cout << "EXAMPLE 3" << '\n';
+  int n = linearFunc(3);
+  cheer(4);
+  cout << "Final result: " << n << '\n';
+
+  cout << "EXAMPLE 4" << '\n';
+  int s1 = collectNumber();
+  int s2 = collectNumber();
+  float h = hypotenuse(s1, s2);
+  printResult(s1, s2, h);
+
+  cout << "EXERCISE" << '\n';
+  printDistance(2, 2, 3, 3);
+
+  return 0;
 }
