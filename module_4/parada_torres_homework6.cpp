25c3587ae739499d85890d0c7bb322260ea94a2a
Jhonatan Parada <jhonatanparada499@gmail.com>
2026-03-22 14:14:01 -0400

    first init

---
+++ module_4/parada_torres_homework6.cpp
@@ -0,0 +1,22 @@
+/*
+Jhonatan Stiven ParadTorres
+March 22, 2026
+*/
+
+#include <cctype>
+#include <cmath>
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+  int *ptr;
+  int var = 7;
+  int foo = 22;
+  ptr = &var;
+
+  cout << ptr << '\n';
+  return 0;
+}
