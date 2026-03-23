2ca6ca558431e5ec0388868cdbdb56233d8278a3
Jhonatan Parada <119643678+jhonatanparada499@users.noreply.github.com>
2026-03-23 14:35:13 +0000

    feat: initial commit

---
+++ module_4/lab_15/lab15_function_parada_torres.cpp
@@ -0,0 +1,14 @@
+/*
+Jhonatan Stiven Parada Torres
+March 23, 2026
+*/
+
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+
+// using std::cerr;
+using std::cin;
+using std::cout;
+using std::endl;
+
---
+++ module_4/lab_15/lab15_main_parada_torres.cpp
@@ -0,0 +1,17 @@
+/*
+Jhonatan Stiven Parada Torres
+March 23, 2026
+*/
+
+#include <iostream>
+#include "lab15_function_parada_torres.cpp"
+
+using std::cerr;
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+
+  return 0;
+}
