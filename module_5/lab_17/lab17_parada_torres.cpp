23d16cc46ecd8415224540ad11cb60ef2ab088e2
Jhonatan Parada <jhonatanparada499@gmail.com>
2026-04-16 18:08:52 -0400

    feat: add sumevennumbers and count negative functions definitions

---
+++ module_5/lab_17/lab17_parada_torres.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using std::cout;
+/*
+Jhonatan Stiven Parada Torres
+April 16, 2026
+*/
+
+int sumEvenNumbers(int theArr[], int theSize) {
+  int sum = 0;
+  for (int i = 0; i < theSize; ++i) {
+    if (theArr[i] % 2 == 0) {
+      sum += theArr[i];
+    }
+  }
+  return sum;
+}
+
+int countNegativeNumbers(int theArr[], int theSize) {
+  int count = 0;
+  for (int i = 0; i < theSize; ++i) {
+    if (theArr[i] < 0) {
+      count++;
+    }
+  }
+  return count;
+}
+
+int main() {
+  int arr[] = {12, -5, 8, -2, 10, -15, 4};
+  int arrSize = sizeof(arr) / sizeof(arr[0]);
+
+  cout << "Sum of even numbers: " << sumEvenNumbers(arr, arrSize) << '\n';
+  cout << "Count of negative numbers: " << countNegativeNumbers(arr, arrSize)
+       << '\n';
+
+  return 0;
+}
